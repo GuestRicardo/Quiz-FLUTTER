29e21af3fd136f17f17996e14ddff61397b5fa44
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-03-17 14:06:13 -0300

    formatando

--- quiz/lib/resposta.dart
+++ quiz/lib/resposta.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-  const Resposta(this.texto, {super.key});
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.texto, this.quandoSelecionado, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        foregroundColor: WidgetStateProperty.all<Color>(
-          const Color.fromARGB(255, 0, 0, 0),
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.blue),
+          foregroundColor: WidgetStateProperty.all(Colors.white),
         ),
+
+        onPressed: quandoSelecionado,
+        child: Text(texto),
       ),
-      
-      onPressed: null,
-      child: Text(texto),
     );
   }
 }
