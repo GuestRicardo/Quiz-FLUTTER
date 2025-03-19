ad149418965cb35049b24d527fe29033abfbfc2a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-03-19 20:59:46 -0300

    formatando

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,29 +1,79 @@
 import 'package:flutter/material.dart';
-import './perguntas.dart';
+import './resultado.dart';
+import './questionario.dart';
 
-void main() => runApp(Comecar());
+void main() => runApp(const PerguntaApp());
 
-const _questionario = PerguntaApp();
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  int _pontuacaoTotal = 0;
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'resposta': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'resposta': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'resposta': [
+        {'texto': 'Maria', 'pontuacao': 10},
+        {'texto': 'João', 'pontuacao': 5},
+        {'texto': 'Leo', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    },
+  ];
 
-class Comecar extends StatelessWidget {
-  const Comecar({super.key});
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+    print(_pontuacaoTotal);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return
-     SizedBox(
-      width: double.infinity,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all(Colors.blue),
-          foregroundColor: WidgetStateProperty.all(Colors.white),
-        ),
-
-        onPressed: () => _questionario,
-        child: const Text('Começar'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Perguntas')),
+        body:
+            temPerguntaSelecionada
+                ? Questionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  quandoResponder: _responder,
+                )
+                : Resultado(_pontuacaoTotal),
       ),
     );
   }
 }
 
-//_questionario,
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
--- quiz/lib/perguntas.dart
+++ quiz/lib/perguntas.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import './resultado.dart';
 import './questionario.dart';
