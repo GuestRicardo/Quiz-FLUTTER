0f4367878b460c1c62adf7912f4755533fefb473
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-03-24 12:53:24 -0300

    adiciona função para reiniciar questionário ao resultado do quiz

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -69,7 +69,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   perguntaSelecionada: _perguntaSelecionada,
                   quandoResponder: _responder,
                 )
-                : Resultado(_pontuacaoTotal),
+                : Resultado(_pontuacaoTotal, _reinicarQuestionario),
       ),
     );
   }
--- quiz/lib/resultado.dart
+++ quiz/lib/resultado.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
   final int pontuacao;
-  const Resultado(this.pontuacao, {super.key});
+  final void Function() quandoReiniciarQuestionario;
+  const Resultado(
+    this.pontuacao,
+    this.quandoReiniciarQuestionario, {
+    super.key,
+  });
 
   String get fraseResultado {
     if (pontuacao < 8) {
